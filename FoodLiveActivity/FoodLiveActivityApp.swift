f55e15ed4100747d387c8811957bc871cded44e7
DavidEtoedia <etoedia@gmail.com>
2023-12-30 13:16:17 -0800

    Initial Commit

---
+++ FoodLiveActivity/FoodLiveActivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodLiveActivityApp.swift
+//  FoodLiveActivity
+//
+//  Created by Inyene Etoedia on 30/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct FoodLiveActivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
